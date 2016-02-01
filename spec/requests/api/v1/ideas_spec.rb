2ed25fe7530b34672d9c0d8ca05ebcb835096c18
Torie Joy-Warren <victoria.joywarren@gmail.com>
2016-02-01 13:54:32 -0700

    Set up rspec

---
+++ spec/requests/api/v1/ideas_spec.rb
@@ -0,0 +1,19 @@
+require "spec_helper"
+
+describe "Ideas API" do
+  it "sends a list of ideas" do
+
+  end
+
+  it "creates an idea" do
+
+  end
+
+  it "updates an idea" do
+
+  end
+
+  it "deletes an idea" do
+
+  end
+end
