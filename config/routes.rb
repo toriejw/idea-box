46cfd89f36c553950c1fc51a0cb4692a03b76970
Torie Joy-Warren <victoria.joywarren@gmail.com>
2016-02-02 12:21:49 -0700

    Fix typo on scss and add json default to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :ideas, only: [:index, :create, :show, :update, :destroy]
+      resources :ideas, only: [:index, :create, :show, :update, :destroy], :defaults => {:format => "json"}
     end
   end
 end
