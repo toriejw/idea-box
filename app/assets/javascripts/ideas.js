1d8aa3b8cb4f35a3744b83ff70ebfbb9b1fd1b27
Torie Joy-Warren <victoria.joywarren@gmail.com>
2016-02-03 21:59:42 -0700

    Show/hide ideas when search queries entered

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -5,7 +5,31 @@ $(document).ready(function(){
   upVoteIdea();
   downVoteIdea();
   editIdeaContent();
+  filterIdeas();
 })
+
+function filterIdeas(){
+  $('#filter-bar').keyup(function(){
+    var $filterQuery = $('#filter-bar').val()
+
+    showFilteredIdeas($filterQuery)
+  })
+}
+
+function showFilteredIdeas(filterQuery) {
+  var $ideas = $('#idea-list').find('.idea')
+
+  $.each($ideas, function (index, idea){
+    ideaTitle = $(idea).find('.idea-title').text()
+    ideaBody = $(idea).find('.idea-body').text()
+
+    if (ideaTitle.indexOf(filterQuery) >= 0 || ideaBody.indexOf(filterQuery) >= 0) {
+      $(idea).show()
+    } else {
+      $(idea).hide()
+    }
+  })
+}
 
 function editIdeaContent() {
   $('#idea-list').on('click', '.edit-btn', function(){
