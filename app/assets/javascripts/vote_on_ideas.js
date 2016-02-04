9f7aaa485c1eb6cca4ed4d530e0b65d75b475a34
Torie Joy-Warren <victoria.joywarren@gmail.com>
2016-02-04 13:29:34 -0700

    Refactor vote_on_ideas.js
    - Extract quality status to constants

--- app/assets/javascripts/vote_on_ideas.js
+++ app/assets/javascripts/vote_on_ideas.js
@@ -1,63 +1,70 @@
 $(document).ready(function(){
-  upVoteIdea();
-  downVoteIdea();
+  upVoteIdea()
+  downVoteIdea()
 })
+
+window.CONSTANTS = {}
+CONSTANTS.swill = {value: 0, name: "swill"}
+CONSTANTS.plausible = {value: 1, name: "plausible"}
+CONSTANTS.genius = {value: 2, name: "genius"}
+
+CONSTANTS.upVoting = {
+  0: CONSTANTS.plausible,
+  1: CONSTANTS.genius,
+  2: CONSTANTS.genius,
+}
+
+CONSTANTS.downVoting = {
+  0: CONSTANTS.swill,
+  1: CONSTANTS.swill,
+  2: CONSTANTS.plausible,
+}
 
 function upVoteIdea() {
   $('#idea-list').on('click', '.up-vote-btn', function(){
-    var $ideaId = $(this).attr('data-id')
-    var $idea = $('.idea[data-id=' + $ideaId + ']')
+    var ideaId = $(this).attr('data-id')
+    var $idea = $('.idea[data-id=' + ideaId + ']')
     var $previousRating = $idea.find('.rating')
-    var $newQualityRating = findNewQualityRatingAfterUpVote($previousRating.text())
+    var newQualityRating = findNewQualityRatingAfterUpVote($previousRating.text())
 
-    updateIdeaRating($ideaId, $newQualityRating, $previousRating)
+    updateIdeaRating(ideaId, newQualityRating, $previousRating)
   })
 }
 
 function downVoteIdea() {
   $('#idea-list').on('click', '.down-vote-btn', function(){
-    var $ideaId = $(this).attr('data-id')
-    var $idea = $('.idea[data-id=' + $ideaId + ']')
+    var ideaId = $(this).attr('data-id')
+    var $idea = $('.idea[data-id=' + ideaId + ']')
     var $previousRating = $idea.find('.rating')
-    var $newQualityRating = findNewQualityRatingAfterDownVote($previousRating.text())
+    var newQualityRating = findNewQualityRatingAfterDownVote($previousRating.text())
 
-    updateIdeaRating($ideaId, $newQualityRating, $previousRating)
+    updateIdeaRating(ideaId, newQualityRating, $previousRating)
   })
 }
 
-function updateIdeaRating(ideaId, newQualityRating, previousRating){
+function updateIdeaRating(ideaId, newQualityRating, $previousRating){
   $.ajax({
     type: 'PUT',
     url: '/api/v1/ideas/' + ideaId,
-    data: {quality: newQualityRating[0]},
-    success: function(response){
-      previousRating.html(" Rating: " + newQualityRating[1])
-    }
+    data: {quality: newQualityRating.value},
+    success: ( function(response){
+      $previousRating.html(" Rating: " + newQualityRating.name)
+    })
   })
 }
 
-function findNewQualityRatingAfterUpVote(previousRating){
-  oldRating = quantifyRating(previousRating)
-
-  if (oldRating === 0) {
-    return [1, "plausible"]
-  } else {
-    return [2, "genius"]
-  }
+function findNewQualityRatingAfterUpVote($previousRating){
+  var oldRating = quantifyRating($previousRating)
+  return CONSTANTS.upVoting[oldRating]
 }
 
-function findNewQualityRatingAfterDownVote(previousRating){
-  oldRating = quantifyRating(previousRating)
-
-  if (oldRating === 2) {
-    return [1, "plausible"]
-  } else {
-    return [0, "swill"]
-  }
+function findNewQualityRatingAfterDownVote($previousRating){
+  var oldRating = quantifyRating($previousRating)
+  return CONSTANTS.downVoting[oldRating]
 }
 
 function quantifyRating(previousRating){
-  oldRating = previousRating.split(" ")[2]
+  var oldRating = previousRating.split(" ")[2]
 
   if (oldRating === "swill") {
     return 0
